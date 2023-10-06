4913e35d469adab986a8d629d7382a51216d1824
mtyszkiewicz <tyszkiewicz.konopqa@gmail.com>
2023-10-06 16:15:00 +0200

    First version of Token Generator

--- src/main/java/tokenGenerator/Main.java
+++ src/main/java/tokenGenerator/Main.java
@@ -1,2 +1,8 @@
-package tokenGenerator;public class Main {
+package tokenGenerator;
+
+public class Main {
+    public static void main(String[] args) {
+        Token token = new Token(10);
+        token.generateToken();
+    }
 }
--- src/main/java/tokenGenerator/Token.java
+++ src/main/java/tokenGenerator/Token.java
@@ -1,2 +1,21 @@
-package tokenGenerator;public class Token {
+package tokenGenerator;
+
+import java.util.Random;
+
+public class Token {
+    int numberOfCharacters;
+    char character;
+
+    public Token(int numberOfCharacters) {
+        this.numberOfCharacters = numberOfCharacters;
+    }
+
+    public void generateToken() {
+        for (int i = 1; i <= numberOfCharacters; i++) {
+            Random random = new Random();
+            character = (char) random.nextInt(33, 126);
+            System.out.print(character);
+        }
+    }
+
 }
